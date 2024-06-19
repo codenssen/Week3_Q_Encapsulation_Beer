7768040a2c2c98730eb27a9f04f29365ffab75ea
ABT Aurelien <aurelien@abrassart.net>
2024-06-19 10:00:03 +0200

    Ajoutez des fichiers projet.

---
+++ Week3_Q_Encapsulation_Beer/BeerEncapsulator.cs
@@ -0,0 +1,61 @@
+﻿namespace Week3_Q_Encapsulation_Beer
+{
+    public class BeerEncapsulator
+    {
+        // Propriétés
+        private double _avalaibleBeerVolume;
+        private int _avalaibleBottles;
+        private int _avalaibleCapsules;
+        public BeerEncapsulator(double avalaibleBeerVolume, int avalaibleBottles, int avalaibleCapsules)
+        {
+            _avalaibleBeerVolume = avalaibleBeerVolume;
+            _avalaibleBottles = avalaibleBottles;
+            _avalaibleCapsules = avalaibleCapsules;
+        }
+
+        public double GetBeerVolume
+        {
+            get { return _avalaibleBeerVolume; }
+        }
+        public int GetBottles
+        {
+            get { return _avalaibleBottles; }
+        }
+        public int GetCapsules
+        {
+            get { return _avalaibleCapsules; }
+        }
+        public double AddBeer(double beer)
+        {
+            _avalaibleBeerVolume += beer;
+            return _avalaibleBeerVolume;
+        }
+        public int ProduceEncapsulatedBeerBottles(int numberOfBottles)
+        {
+            if (_avalaibleBeerVolume < numberOfBottles * 33)
+            {
+                Console.WriteLine($"Pas assez de bière. Rajouter {(numberOfBottles * 33) - _avalaibleBeerVolume} centilitres de bière.");
+                return 0;
+            }
+            else if (_avalaibleBottles < numberOfBottles)
+            {
+                Console.WriteLine($"Pas assez de bouteilles. Rajouter {numberOfBottles - _avalaibleBottles} bouteilles.");
+                return 0;
+            }
+            else if (_avalaibleCapsules < numberOfBottles)
+            {
+                Console.WriteLine($"Pas assez de capsules. Rajouter {numberOfBottles - _avalaibleCapsules} capsules.");
+                return 0;
+            }
+            else
+            {
+                _avalaibleBeerVolume -= numberOfBottles * 33;
+                _avalaibleBottles -= numberOfBottles;
+                _avalaibleCapsules -= numberOfBottles;
+                return numberOfBottles;
+            }
+
+
+        }
+    }
+}
---
+++ Week3_Q_Encapsulation_Beer/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Week3_Q_Encapsulation_Beer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double avalaibleBeerVolume;
+            int avalaibleBottles;
+            int avalaibleCapsules;
+            
+            Console.WriteLine("Veuillez rentrer vos données :");
+            Console.WriteLine("Volume de bière disponible (en centilitres) ?");
+            avalaibleBeerVolume = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nombre de bouteille disponible ?");
+            avalaibleBottles = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nombre de capsule disponible ?");
+            avalaibleCapsules = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Voici votre demande : {avalaibleBeerVolume} litres de bière à embouteiller, {avalaibleBottles} bouteilles disponibles, {avalaibleCapsules} capsules disponibles");
+            
+            BeerEncapsulator usine1 = new BeerEncapsulator(avalaibleBeerVolume, avalaibleBottles, avalaibleCapsules);
+            Console.WriteLine("");
+            Console.WriteLine("Combien de bouteilles souhaitez vous");
+            Console.WriteLine($"Status du stock : {usine1.GetBeerVolume} centilitres de bières, {usine1.GetBottles} bouteilles, {usine1.GetCapsules} capsules.");
+            int bouteilles = Convert.ToInt32(Console.ReadLine());
+            usine1.ProduceEncapsulatedBeerBottles(bouteilles);
+
+        }
+    }
+}
